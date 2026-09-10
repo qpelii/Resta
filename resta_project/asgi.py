f444c3ed655e716018cd4af893009e18e7919ed7
qpelii <qelichkhaniparsa@gmail.com>
2026-09-11 03:13:19 +0330

    set default file django

--- resta_project/asgi.py
+++ resta_project/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for shop project.
+ASGI config for resta_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shop.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resta_project.settings')
 
 application = get_asgi_application()
---
+++ shop/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RestaProjectConfig(AppConfig):
+    name = "resta_project"
